71dac89baa9f4d0450116d75523ddc3e30dd12d7
Kaito <sigeru20020310@gmail.com>
2026-04-09 22:29:56 +0900

    プロフィール更新のパスワード不要化、ドロップダウンメニュー実装、各ページの日本語化完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_up_path_for(resource)
-    root_path
-  end
-  
   protected
 
   def configure_permitted_parameters
-    # 新規登録(sign_up)の時に name と introduction を許可する
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :introduction])
-    # アカウント更新(account_update)の時も許可する
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :introduction])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :introduction, :avatar])
+  end
+
+  # Deviseの標準メソッドを完全に上書きしてパスワードチェックをスルー
+  def update_resource(resource, params)
+    resource.update_without_password(params)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,25 @@ class User < ApplicationRecord
   has_one_attached :avatar
   has_many :rooms, dependent: :destroy
   has_many :reservations, dependent: :destroy
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+
+# パスワードが空の場合、現在のパスワードなしで更新を許可する設定
+  def update_with_password(params, *options)
+    if params[:password].blank? && params[:password_confirmation].blank? && params[:current_password].blank?
+      params.delete(:current_password)
+      params.delete(:password)
+      params.delete(:password_confirmation)
+      
+      # パスワード関連を除外して直接更新
+      result = update(params, *options)
+      clean_up_passwords
+      return result
+    end
+    # パスワードが入力されている場合は通常のDeviseの動き（バリデーションあり）にする
+    super
+  end
 end
